6756c074e10178525004002f6656a5976a3ee02c
Manu Vijayan <mn.vjyn@gmail.com>
2017-03-17 23:54:43 +0530

    Ecommerce project

---
+++ Backup1703/YuzuBackEnd/src/main/java/com/yuzu/domain/Product.java
@@ -0,0 +1,66 @@
+package com.yuzu.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "product")
+@Component
+public class Product {
+
+	@Id
+	private String id;
+	private String name;
+
+	private float price;
+	private String description;
+
+	// private String category_id;
+	// private String supplier_id;
+
+	public Product() {
+
+	}
+	// getters + setters
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + ", description=" + description + "]";
+	}
+
+}
